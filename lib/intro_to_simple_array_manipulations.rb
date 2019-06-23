c84c60b28371ad4348a6c8dd013f266ecd38095e
Clara Yip <clara.yip1@gmail.com>
2019-06-23 19:00:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,4 +4,5 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift()
+def using_unshift(bouroughs_in_nyc, )
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
