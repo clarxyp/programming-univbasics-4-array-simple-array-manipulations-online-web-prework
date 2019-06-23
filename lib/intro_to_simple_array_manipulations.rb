7e8f24ba0515204506a18364fb6a456151741f69
Clara Yip <clara.yip1@gmail.com>
2019-06-23 21:09:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,12 +11,9 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end
 
 def using_pop(continents)
-   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-   continents.pop
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  selected_continents = continents.pop
+  selected_continents.size
 end
 
-def using_pop(continents)
-   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-   continents.pop.size
-end
 
